d618f4e304fcc6fc3f646e8a67f7050a2ce31da3
Javier Marquez <javi@arqex.com>
2022-07-11 19:11:50 +0200

    Clean up unused preact boilerplate

---
+++ demo_site/components/app.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent, h } from 'preact';
+
+const App: FunctionalComponent = () => {
+    return (
+        <div id="preact_root">
+            Este es nuestro playground
+        </div>
+    );
+};
+
+export default App;
